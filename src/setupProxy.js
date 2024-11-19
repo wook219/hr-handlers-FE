97cebbfd8931cbb98d0b10c0536aa6600bd26946
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-11-19 18:57:58 +0900

    CORS 에러 해결

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://localhost:8080/api',
+            target: 'http://localhost:8080/api',
             changeOrigin: true,
         })
     );
