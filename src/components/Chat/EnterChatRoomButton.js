0fc9006de264b25ebdb3e49a47df2b8a7d045d68
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-11-24 22:55:58 +0900

    Feat : 채팅방 목록 페이지 추가

---
+++ src/components/Chat/EnterChatRoomButton.js
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom'; // useNavigate로 페이지 이동
+import React from 'react';
+import './EnterChatRoomButton.css';
+
+const EnterChatRoomButton = ({ chatRoomId }) => {
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  const handleEnterChatRoomClick = () => {
+    navigate(`/chatroom/${chatRoomId}`); // 페이지 이동
+  };
+
+  return (
+    <button className="enter-button" onClick={handleEnterChatRoomClick}>
+      참여
+    </button>
+  );
+};
+
+export default EnterChatRoomButton;
---
+++ src/pages/Chat/useWebSocket.js
@@ -0,0 +1,4 @@
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+const useWebSocket = () => {};
