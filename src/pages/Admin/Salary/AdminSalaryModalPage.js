a88765ce0831994aefb00650382038abc20041b7
[CLOUD4] <dbslxl11@gmail.com>
2024-11-25 15:27:33 +0900

    Feat: 변수에 타입설정하여 공통 form 항목 반복문으로 처리

--- src/pages/Admin/Salary/AdminSalaryModalPage.js
+++ src/pages/Admin/Salary/AdminSalaryModalPage.js
@@ -6,48 +6,61 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const AdminSalaryModalPage = (props) => {
-    const [formData, setFormData] = useState({
-        position: '',
-        deptName: '',
-        name: '',
-        basicSalary: '',
-        deduction: '',
-        netSalary: '',
-        payDate: '',
-    });
+    const [formData, setFormData] = useState([
+        { key: 'position', value: '', label: '직위', type: 'select', isDisable: false },
+        { key: 'deptName', value: '', label: '부서', type: 'select', isDisable: false },
+        { key: 'name', value: '', label: '이름', type: 'select', isDisable: false },
+        { key: 'basicSalary', value: '', label: '지급총액', type: 'input', isDisable: false },
+        { key: 'deduction', value: '', label: '공제총액', type: 'input', isDisable: false },
+        { key: 'netSalary', value: '', label: '실지급액', type: 'input', isDisable: false },
+        { key: 'payDate', value: '', label: '급여일', type: 'custom', isDisable: false },
+    ]);
 
-    const [selectedDate, setSelectedDate] = useState({
-        year: '',
-        month: '',
-        day: ''
-    });
+    const [selectedDate, setSelectedDate] = useState([
+        { key: 'year', value: '', label: '급여연도', type: 'select', isDisable: true },
+        { key: 'month', value: '', label: '급여월', type: 'select', isDisable: false },
+        { key: 'day', value: '', label: '급여일', type: 'select', isDisable: true },
+    ]);
 
     useEffect(() => {
         if (props.formData) {
-            console.log('props.formData : ', props.formData);
-            setFormData({
-                ...props.formData
-            });
+            setFormData(prevFormData =>
+                prevFormData.map(field => {
+                    const updatedField = props.formData.find(item => item.key === field.key);
+                    return updatedField ? { ...field, value: updatedField.value, options: updatedField.options } : field;
+                })
+            );
         }
     }, [props.formData]);
 
+    useEffect(() => {
+        if (props.selectedDate) {
+            setSelectedDate(prevSelectedDate =>
+                prevSelectedDate.map(field => {
+                    const updatedField = props.selectedDate.find(item => item.key === field.key);
+                    return updatedField ? { ...field, value: updatedField.value, options: updatedField.options } : field;
+                })
+            );
+        }
+    }, [props.selectedDate]);
+
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData(prevFormData => ({
-            ...prevFormData,
-            [name]: value
-        }));
+        setFormData((prevFormData) =>
+            prevFormData.map((field) =>
+                field.key === name ? { ...field, value } : field
+            )
+        );
     };
 
     const handleDateChange = (e) => {
         const { name, value } = e.target;
-        setSelectedDate(prevSelectedDate => ({
-            ...prevSelectedDate,
-            [name]: value
-        }));
+        setSelectedDate((prevSelectedDate) =>
+            prevSelectedDate.map((field) =>
+                field.key === name ? { ...field, value } : field
+            )
+        );
     };
-
-    
 
     return (
         <Modal
@@ -62,118 +75,53 @@ const AdminSalaryModalPage = (props) => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">부서</Form.Label>
-                    <Form.Select
-                        id="position"
-                        name="position"
-                        value={formData.position}
-                        onChange={handleChange}
-                    >
-                        <option>개발팀</option>
-                        <option>재무팀</option>
-                    </Form.Select>
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">직위</Form.Label>
-                    <Form.Select
-                        id="deptName"
-                        name="deptName"
-                        value={formData.deptName}
-                        onChange={handleChange}
-                    >
-                        <option>사원</option>
-                        <option>대리</option>
-                        <option>팀장</option>
-                    </Form.Select>
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">이름</Form.Label>
-                    <Form.Select
-                        id="name"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                    >
-                        <option>홍길동</option>
-                        <option>엘리스</option>
-                        <option>김철수</option>
-                    </Form.Select>
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">지급총액</Form.Label>
-                    <Form.Control
-                        type="number"
-                        name="basicSalary"
-                        value={formData.basicSalary}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">공제총액</Form.Label>
-                    <Form.Control
-                        type="number"
-                        name="deduction"
-                        value={formData.deduction}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">실지급액</Form.Label>
-                    <Form.Control
-                        type="number"
-                        name="netSalary"
-                        value={formData.netSalary}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">급여연도</Form.Label>
-                    <Form.Select
-                        id="year"
-                        name="year"
-                        value={selectedDate.year}
-                        onChange={handleDateChange}
-                        disabled
-                    >
-                        <option>2024</option>
-                    </Form.Select>
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">급여월</Form.Label>
-                    <Form.Select
-                        id="month"
-                        name="month"
-                        value={selectedDate.month}
-                        onChange={handleDateChange}
-                    >
-                        <option></option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                        <option>10</option>
-                        <option>11</option>
-                        <option>12</option>
-                    </Form.Select>
-                </div>
-                <div>
-                    <Form.Label htmlFor="inputPassword5">급여일</Form.Label>
-                    <Form.Select
-                        id="day"
-                        name="day"
-                        value={selectedDate.day}
-                        onChange={handleDateChange}
-                        disabled
-                    >
-                        <option>13</option>
-                    </Form.Select>
-                </div>
+                {formData.map((field) => (
+                    <div key={field.key}>
+                        <Form.Label htmlFor={field.key}>{field.label}</Form.Label>
+                        {field.type === 'select' ? (
+                            <Form.Select
+                                id={field.key}
+                                name={field.key}
+                                value={field.value}
+                                onChange={handleChange}
+                                disabled={field.isDisable}
+                            >
+                                {field.options?.map((option, index) => (
+                                    <option key={index} value={option}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </Form.Select>
+                        ) : field.type === 'input' ? (
+                            <Form.Control
+                                type="number"
+                                name={field.key}
+                                value={field.value}
+                                onChange={handleChange}
+                            />
+                        ) : null}
+                    </div>
+                ))}
+                {selectedDate.map((field) => (
+                    <div key={field.key}>
+                        <Form.Label htmlFor={field.key}>{field.label}</Form.Label>
+                        {field.type === 'select' ? (
+                            <Form.Select
+                                id={field.key}
+                                name={field.key}
+                                value={field.value}
+                                onChange={handleDateChange}
+                                disabled={field.isDisable}
+                            >
+                                {field.options?.map((option, index) => (
+                                    <option key={index} value={option}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </Form.Select>
+                        ) : null}
+                    </div>
+                ))}
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={props.onHide}>
--- src/pages/Admin/Salary/AdminSalaryPage.js
+++ src/pages/Admin/Salary/AdminSalaryPage.js
@@ -12,43 +12,70 @@ import AdminSalaryModalPage from "./AdminSalaryModalPage";
 const AdminSalaryPage = () => {
     const [salaries, setSalaries] = useState([]);
     const [modalShow, setModalShow] = useState(false);
-    const [formData, setFormData] = useState({
-        position: '',
-        deptName: '',
-        name: '',
-        basicSalary: '',
-        deduction: '',
-        netSalary: '',
-        payDate: '',
-    });
+    const [modalType, setModalType] = useState('');
+    const [formData, setFormData] = useState([
+        { key: 'position', value: '', label: '직위', type: 'select', options: [], isDisable: false },
+        { key: 'deptName', value: '', label: '부서', type: 'select', options: [], isDisable: false },
+        { key: 'name', value: '', label: '이름', type: 'select', options: [], isDisable: false },
+        { key: 'basicSalary', value: '', label: '지급총액', type: 'input', isDisable: false },
+        { key: 'deduction', value: '', label: '공제총액', type: 'input', isDisable: false },
+        { key: 'netSalary', value: '', label: '실지급액', type: 'input', isDisable: false },
+        { key: 'payDate', value: '', label: '급여일', type: 'custom', isDisable: false },
+    ]);
+
+    const [selectedDate, setSelectedDate] = useState([
+        { key: 'year', value: '', label: '급여연도', type: 'select', options: [2024], isDisable: true },
+        { key: 'month', value: '', label: '급여월', type: 'select', options: [1,2,3,4,5,6,7,8,9,10,11,12], isDisable: false },
+        { key: 'day', value: '', label: '급여일', type: 'select', options: [13], isDisable: true },
+    ]);
 
     useEffect(() => {
         getSalaries()
     }, []);
 
+    // 추가 버튼 클릭시 실행되는 함수
     const handleCreate = () => {
-        setFormData({  // formData를 초기화
-            position: '',
-            deptName: '',
-            name: '',
-            basicSalary: '',
-            deduction: '',
-            netSalary: '',
-            payDate: '',
-        });
+        setFormData([  // formData를 초기화
+            { key: 'position', value: '', label: '직위', type: 'select', options: ['사원', '대리', '팀장'], isDisable: false },
+            { key: 'deptName', value: '', label: '부서', type: 'select', options: ['재무팀', '개발팀'], isDisable: false },
+            { key: 'name', value: '', label: '이름', type: 'select', options: ['홍길동', '김철수', '엘리스'], isDisable: false },
+            { key: 'basicSalary', value: '', label: '지급총액', type: 'input', isDisable: false },
+            { key: 'deduction', value: '', label: '공제총액', type: 'input', isDisable: false },
+            { key: 'netSalary', value: '', label: '실지급액', type: 'input', isDisable: false },
+            { key: 'payDate', value: '', label: '급여일', type: 'custom', isDisable: false },
+        ]);
+        setSelectedDate([  // SelectedDate 초기화
+            { key: 'year', value: '', label: '급여연도', type: 'select', options: ['2024'], isDisable: true },
+            { key: 'month', value: '', label: '급여월', type: 'select', options: ['1','2','3','4','5','6','7','8','9','10','11','12'], isDisable: false },
+            { key: 'day', value: '', label: '급여일', type: 'select', options: ['13'], isDisable: true },
+        ]);
+
+        setModalType('create');
         setModalShow(true);
     };
 
     // 추가, 수정 api
     const handleSubmit = async (data, selectedDate) => {
-        
-        const params = {
-            employeeId: 1,
-            basicSalary: data.basicSalary,
-            deduction: data.deduction,
-            netSalary: data.netSalary,
-            payDate: "2024" + "-"+ (selectedDate.month).toString().padStart(2, '0') + "-" + "13"
-        };
+        let params = {};
+
+        // 급여 정보 데이터 세팅
+        data.forEach((item) => {
+            if (item.value) {
+                params[item.key] = item.value;
+            }
+        });
+
+        // 급여일 데이터 세팅
+        const year = "2024";
+        const month = selectedDate.find(item => item.key === "month")?.value || "01";
+        const day = "13";
+
+        // api에 전달한 데이터 세팅
+        params = {
+            ...params,
+            employeeId: 1, // todo 아이디는 나중에
+            payDate: `${year}-${month.toString().padStart(2, '0')}-${day}`
+        }
 
         console.log("data : ", data);
         console.log("selectedDate : ", selectedDate);
@@ -143,6 +170,8 @@ const AdminSalaryPage = () => {
                 onHide={() => setModalShow(false)}
                 handleSubmit={handleSubmit}
                 formData={formData}
+                selectedDate={selectedDate}
+                modalType={modalType}
             />
         </div>
     );
