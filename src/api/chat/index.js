c77d74fda3311ba43c5882509afc89b25c505824
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-11-27 21:39:03 +0900

    Feat : 프론트 웹 소켓 시큐리티 적용

--- src/api/chat/index.js
+++ src/api/chat/index.js
@@ -12,7 +12,25 @@ export const getAllChatRoomAPI = async () => {
 };
 
 // 참여 중인 채팅방 목록 조회 API
-export const getAllEnterChatAPI = async () => {};
+export const getAllEnterChatAPI = async () => {
+  try {
+    const token = localStorage.getItem('access_token');
+
+    if (!token) {
+      throw new Error('JWT 토큰이 없습니다. 로그인을 해주세요.');
+    }
+
+    const response = await axios.get('/chat', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch EnterChatRooms:', error);
+    throw error;
+  }
+};
 
 // 채팅방 생성 API
 export const createChatRoomAPI = async (chatRoomTitle) => {
@@ -22,6 +40,29 @@ export const createChatRoomAPI = async (chatRoomTitle) => {
     return response.data;
   } catch (error) {
     console.error('Failed to create chatRoom: ', error);
+    throw error;
+  }
+};
+
+export const getChatMessagesAPI = async (roomId) => {
+  try {
+    const token = localStorage.getItem('access_token');
+
+    if (!token) {
+      throw new Error('JWT 토큰이 없습니다. 로그인을 해주세요.');
+    }
+
+    // axios로 채팅 내역 조회 요청
+    const response = await axios.get(`/chatroom/${roomId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    // 받은 데이터가 정상적으로 response.data에 포함되어 있음
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch chat messages:', error);
     throw error;
   }
 };
--- src/pages/Chat/ChatMessage.js
+++ src/pages/Chat/ChatMessage.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 // 보낸 메시지 받은 메시지 구분
 
-const ChatMessage = ({ message }) => {
-  return <div></div>;
+const ChatMessage = ({ message, name }) => {
+  return (
+    <div>
+      <div>
+        <span>{name} : </span>
+        {message}
+      </div>
+    </div>
+  );
 };
+
+export default ChatMessage;
